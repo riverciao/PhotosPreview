3b6678176585e634f221f816e32ad9ed1adb5f45
Ciao Huang <riverciao@gmail.com>
2018-07-04 18:04:55 +0800

    add album cell

---
+++ PhotosPreview/AlbumCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlbumCell.swift
+//  PhotosPreview
+//
+//  Created by riverciao on 2018/7/4.
+//  Copyright © 2018年 riverciao. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
