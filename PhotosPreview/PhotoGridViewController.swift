58c57340abf349380af7977b87c056f5c93d0461
Ciao Huang <riverciao@gmail.com>
2018-07-10 12:32:16 +0800

    add back

---
+++ PhotosPreview/PhotoGridViewController.swift
@@ -0,0 +1,197 @@
+//
+//  PhotoGridViewController.swift
+//  PhotosPreview
+//
+//  Created by riverciao on 2018/6/25.
+//  Copyright © 2018年 riverciao. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class PhotoGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    var assets = [PHAsset]()
+    var imageManager = ImageAPIManager()
+    
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var albumButton: UIButton!
+    var isAlbumSelected = false
+    @IBOutlet weak var albumTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let cameraRollCollection = imageManager.cameraRollAssetCollection() {
+            imageManager.fetchAssets(in: cameraRollCollection)
+            albumButton.setTitle(cameraRollCollection.localizedTitle, for: .normal)
+        }
+        setup()
+    }
+
+    // MARK: Setup
+    
+    private func setup() {
+        
+        // MARK: CollectionView
+        collectionView.register(
+            UINib(nibName: PhotoGridCell.identifier, bundle: nil),
+            forCellWithReuseIdentifier: PhotoGridCell.identifier
+        )
+        collectionView.register(
+            UINib(nibName: PhotoGridHeaderCell.identifier, bundle: nil),
+            forSupplementaryViewOfKind: UICollectionElementKindSectionHeader,
+            withReuseIdentifier: PhotoGridHeaderCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        // MARK: CloseButton
+        closeButton.addTarget(self, action: #selector(close), for: .touchUpInside)
+        closeButton.addShadow(
+            shadowColor: .black,
+            shadowOffset: CGSize(width: 0.5, height: 0.5),
+            opacity: 1.0,
+            shadowRadius: 1
+        )
+        
+        // MARK: AlbumButton
+        albumButton.addTarget(self, action: #selector(selectAlbum), for: .touchUpInside)
+        albumButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        albumButton.titleLabel?.numberOfLines = 0
+        albumButton.titleLabel?.addShadow(
+            shadowColor: .black,
+            shadowOffset: CGSize(width: 0.5, height: 0.5),
+            opacity: 1.0,
+            shadowRadius: 1
+        )
+        
+        // MARK: AlbumTableView
+        albumTableView.register(
+            UINib(nibName: AlbumCell.identifier, bundle: nil),
+            forCellReuseIdentifier: AlbumCell.identifier
+        )
+        albumTableView.addDarkBlurBackground()
+        albumTableView.separatorInset = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+        albumTableView.dataSource = self
+        albumTableView.delegate = self
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageManager.assetsInColletion.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoGridCell.identifier, for: indexPath) as! PhotoGridCell
+        let asset = imageManager.assetsInColletion[indexPath.row]
+        if cell.tag != 0 {
+            imageManager.cancelImageRequest(cell.tag)
+        }
+        let size = CGSize(width: cell.bounds.width * 2, height: cell.bounds.height * 2)
+        cell.tag = imageManager.requsetImage(for: asset, targetSize: size, resultHandler: { (image) in
+            cell.imageView.image = image
+        })
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: PhotoGridHeaderCell.identifier, for: indexPath)
+        return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return headerView.bounds.size
+    }
+    
+    // MARK: UICollectionViewDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let asset = imageManager.assetsInColletion[indexPath.row]
+        NotificationCenter.default.post(name: Notification.Name("image"), object: asset)
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.width
+        let width = screenWidth / 3
+        let height = width
+        return CGSize(width: width, height: height)
+    }
+
+    // MARK: Action
+    
+    @objc private func close(_ sender: UIButton) {
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc private func selectAlbum(_ sender: UIButton) {
+        if !isAlbumSelected {
+            openAlbumView()
+            isAlbumSelected = true
+        } else {
+            closeAlbumView()
+            isAlbumSelected = false
+        }
+    }
+    
+    /// Animate to open the albumTableView
+    private func openAlbumView() {
+        albumTableView.translatesAutoresizingMaskIntoConstraints = true
+        UIView.animate(withDuration: 0.3) {
+            self.albumTableView.frame.origin.y = self.headerView.frame.maxY
+            self.headerView.backgroundColor = .photoGridHeaderViewOpenedColor
+        }
+    }
+    
+    /// Animate to close the albumTableView
+    private func closeAlbumView() {
+        UIView.animate(withDuration: 0.3) {
+            self.albumTableView.frame.origin.y = self.view.frame.maxY
+            self.headerView.backgroundColor = .photoGridHeaderViewClosedColor
+        }
+    }
+}
+
+extension PhotoGridViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageManager.assetCollections.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: AlbumCell.identifier, for: indexPath) as! AlbumCell
+        let assetCollection = imageManager.assetCollections[indexPath.row]
+        cell.albumNameLabel.text = assetCollection.localizedTitle
+        cell.albumAssetNumberLabel.text = String(assetCollection.photosCount)
+        let size = CGSize(width: cell.bounds.width * 2, height: cell.bounds.height * 2)
+        imageManager.latestThumbnailImage(in: assetCollection, at: size) { (image) in
+            cell.albumImageView.image = image
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 106
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let colletcion = imageManager.assetCollections[indexPath.row]
+        imageManager.fetchAssets(in: colletcion)
+        collectionView.reloadData()
+        let assetCollection = imageManager.assetCollections[indexPath.row]
+        albumButton.setTitle(assetCollection.localizedTitle, for: .normal)
+        isAlbumSelected = false
+        closeAlbumView()
+    }
+}
---
+++ PhotosPreview/PreviewController.swift
@@ -0,0 +1,228 @@
+//
+//  PreviewController.swift
+//  PhotosPreview
+//
+//  Created by riverciao on 2018/6/25.
+//  Copyright © 2018年 riverciao. All rights reserved.
+//
+
+import UIKit
+
+class PreviewController: UIViewController {
+
+    // MARK: Properties
+    
+    var imageManager = ImageAPIManager()
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var displayImageView: UIImageView!
+    @IBOutlet weak var placeholderImageView: UIImageView!
+    @IBOutlet weak var previewButton: UIButton!
+    @IBOutlet weak var previewView: UIView!
+    var isPreviewOpened = false
+    @IBAction func openPreviewView(_ sender: UIButton) {
+        if !isPreviewOpened {
+            openPreview()
+            isPreviewOpened = true
+        } else {
+            closePreview()
+            isPreviewOpened = false
+        }
+        updateUI()
+    }
+    
+    @IBOutlet weak var photoGridButton: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var imagePlaceholderImageView: UIImageView!
+    @IBOutlet weak var previewViewTopConstraint: NSLayoutConstraint!
+    
+    // MARK: View life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        imageManager.fetchAssetsInCameraRoll()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        closePreview()
+        isPreviewOpened = false
+        updateUI()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateMinimumZoomScaleForSize()
+    }
+    
+    // MARK: Setup
+    
+    private func setup() {
+        // MARK: CollectionView
+        collectionView.register(UINib(nibName: PhotoGridCell.identifier, bundle: nil), forCellWithReuseIdentifier: PhotoGridCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.contentInsetAdjustmentBehavior = .never
+        
+        // MARK: Button
+        photoGridButton.setupButtonUI(
+            with: #imageLiteral(resourceName: "icon-grid"),
+            backgroundColor: .photoGridButtonBackgroundColor,
+            tintColor: .photoGridButtonTintColor,
+            conerRadius: photoGridButton.bounds.width / 2
+        )
+        previewButton.setupButtonUI(
+            with: #imageLiteral(resourceName: "icon-photo"), backgroundColor: .unselectButtonBackgroundColor,
+            tintColor: .unselectButtonTintColor,
+            conerRadius: 5
+        )
+        
+        // MARK: ScrollView
+        scrollView.delegate = self
+        scrollView.contentSize = displayImageView.bounds.size
+        scrollView.maximumZoomScale = 2.0
+        scrollView.contentInset = .zero
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        
+        // MARK: ImageView
+        imagePlaceholderImageView.image = #imageLiteral(resourceName: "icon-photo").withRenderingMode(.alwaysTemplate)
+        imagePlaceholderImageView.tintColor = .white
+        
+        // MARK: TapGesture
+//        let tap = UITapGestureRecognizer(target: self, action: #selector(closePreviewView))
+//        view.addGestureRecognizer(tap)
+        
+        // MARK: Notification
+        NotificationCenter.default.addObserver(self, selector: #selector(loadImage), name: Notification.Name("image"), object: nil)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    // MARK: Action
+    
+    @objc private func loadImage(_ sender: Notification) {
+        let size = CGSize(width: displayImageView.bounds.width * 2, height: displayImageView.bounds.height * 2)
+        if let asset = imageManager.asAsset(sender.object) {
+            imageManager.requsetImage(for: asset, targetSize: size) { (image) in
+                self.displayImageView.image = image
+                self.placeholderImageView.image = nil
+                self.closePreview()
+                self.isPreviewOpened = false
+                self.updateUI()
+            }
+        }
+    }
+    
+    private func updateMinimumZoomScaleForSize() {
+        let size = scrollView.bounds.size
+        let widthScale = size.width / displayImageView.bounds.width
+        let heightScale = size.height / displayImageView.bounds.height
+        let minimunScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minimunScale
+        scrollView.zoomScale = minimunScale
+        
+    }
+    
+    private func updateUI() {
+        if isPreviewOpened {
+            previewButton.backgroundColor = .selectedButtonBackgroundColor
+            previewButton.tintColor = .selectedButtonTintColor
+            photoGridButton.isHidden = false
+        } else {
+            previewButton.backgroundColor = .unselectButtonBackgroundColor
+            previewButton.tintColor = .unselectButtonTintColor
+            photoGridButton.isHidden = true
+        }
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "GoToPhotoGrid" {
+            if let photoGridViewController = segue.destination as? PhotoGridViewController {
+                imageManager.fetchAssetCollections()
+                photoGridViewController.imageManager = imageManager
+            }
+        }
+    }
+    
+
+    func openPreview() {
+        if !isPreviewOpened {
+            previewView.translatesAutoresizingMaskIntoConstraints = true
+            UIView.animate(withDuration: 0.3) {
+                self.previewView.frame.origin.y = self.view.bounds.maxY - self.previewView.frame.height
+                self.previewButton.frame.origin.y = self.view.bounds.maxY - self.previewView.frame.height - self.previewButton.frame.height
+            }
+        }
+    }
+    
+    @objc func closePreview(_ sender: UITapGestureRecognizer? = nil) {
+        if isPreviewOpened {
+            UIView.animate(withDuration: 0.3) {
+                self.previewView.frame.origin.y = self.view.bounds.maxY
+                self.previewButton.frame.origin.y = self.view.bounds.maxY - self.previewButton.frame.height
+            }
+        }
+    }
+}
+
+extension PreviewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate {
+    
+    // MARK: UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageManager.numberOfAssets()
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoGridCell.identifier, for: indexPath) as! PhotoGridCell
+        let asset = imageManager.asset(at: indexPath)
+        
+        if cell.tag != 0 {
+            imageManager.cancelImageRequest(cell.tag)
+        }
+        let size = CGSize(width: cell.bounds.width * 2, height: cell.bounds.height * 2)
+        cell.tag = imageManager.requsetImage(for: asset, targetSize: size, resultHandler: { (image) in
+            cell.imageView.image = image
+        })
+        return cell
+    }
+    
+    // MARK: UICollectionViewDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let asset = imageManager.asset(at: indexPath)
+        let size = CGSize(width: displayImageView.bounds.width * 2, height: displayImageView.bounds.height * 2)
+        imageManager.requsetImage(for: asset, targetSize: size) { (image) in
+            self.displayImageView.image = image
+            self.placeholderImageView.image = nil
+            self.closePreview()
+            self.isPreviewOpened = false
+            self.updateUI()
+        }
+    }
+    
+    // MARK: UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width / 3
+        let height = width
+        return CGSize(width: width, height: height)
+    }
+    
+    
+    // MARK: UIScrollViewDelegate
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return displayImageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5 , 0.0)
+        let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5 , 0.0)
+        scrollView.contentInset = .init(top: offsetY, left: offsetX, bottom: 0, right: 0)
+    }
+}
+
